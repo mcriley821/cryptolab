6e9ca973fd24fa0050fdca93a6ecc2c3cd6d80b7
Matthew <mcriley821@gmail.com>
2025-10-25 12:34:51 -0700

    add caesar crack and brute_force

--- cryptolab/substitution/caesar.py
+++ cryptolab/substitution/caesar.py
@@ -2,6 +2,10 @@
 https://en.wikipedia.org/wiki/Caesar_cipher
 """
 
+from ..utils.analysis import chi_squared
+
+from typing import Callable, Iterator
+
 
 def encrypt(
     text: str,
@@ -101,14 +105,78 @@ def decrypt(
     return ret
 
 
+def brute_force(ciphertext: str) -> Iterator[str]:
+    """
+    Brute force decrypt the ciphertext.
+
+    Parameters
+    ----------
+    ciphertext : str
+        The ciphertext to brute force decrypt.
+
+    Returns
+    -------
+    Iterator[str]
+        An iterator yielding all possible Caesar decryptions.
+    """
+    for i in range(26):
+        yield decrypt(ciphertext, i)
+
+
+def crack(ciphertext: str, *, score: Callable[[str], float] = chi_squared) -> str:
+    """
+    Crack the decryption of the ciphertext using the score function.
+
+    Parameters
+    ----------
+    ciphertext : str
+        The ciphertext to crack.
+
+    score : Callable[[str], float], default=chi_squared
+        The score function which treats lower values as more likely to be a
+        valid decryption.
+
+    Returns
+    -------
+    str
+        The lowest scoring decryption.
+
+    Examples
+    --------
+    >>> crack("iq mdq pueoahqdqp rxqq mf azoq")
+    'we are discovered flee at once'
+
+    Notes
+    -----
+    TODO: replace the score function type with one from a scoring module
+    """
+    top: tuple[str, float] = ("", float("inf"))
+
+    for text in brute_force(ciphertext):
+        if (sc := score(text)) < top[1]:
+            top = (text, sc)
+
+    return top[0]
+
+
 if __name__ == "__main__":
+    from random import randint
+
     plaintext = "we are discovered flee at once"
-    shift = 5
+    shift = randint(1, 25)
 
     enc = encrypt(plaintext, shift)
     print(enc, "\n")
 
     dec = decrypt(enc, shift)
-    print(dec)
+    print(dec, "\n")
+
+    cracked = crack(enc)
+    print(cracked, "\n")
+
+    for i in brute_force(enc):
+        print(i)
+    print()
 
     assert dec == plaintext
+    assert dec == cracked
