fa6fd3c6ac71c5565e005b7056e6c0883d1816ea
mcriley821 <mcriley821@gmail.com>
2025-11-13 02:13:45 -0600

    copy lazy loading technique from words module

--- cryptolab/scoring/ngram.py
+++ cryptolab/scoring/ngram.py
@@ -1,87 +1,176 @@
-from importlib.resources import open_text
+from dataclasses import dataclass, field
+from importlib.resources import files
 from math import log10
 
-# ngram data holds the ngram lookup table and floor value
-_ngram_data = tuple[dict[str, float], float]
+
+@dataclass(slots=True)
+class _NgramScorer:
+    """
+    Lazy loading ngram score data class.
+
+    Parameters
+    ----------
+    file_name : str
+        The file name of the ngram data.
+    """
+
+    _file_name: str
+    _loaded: bool = False
+
+    _data: dict[str, float] = field(default_factory=dict[str, float])
+
+    # default score when ngram is not in the data (lazy loaded)
+    _floor: float = float("NaN")
+
+    # ngram length (lazy loaded)
+    _ngram_len: int = 0
+
+    def _load_data(
+        self,
+        *,
+        module: str = "cryptolab.scoring.data",
+    ):
+        """
+        Load the ngram data from the file.
+
+        Parameters
+        ----------
+        module : str,default="cryptolab.scoring.data"
+            The module to find the data file.
+        """
+        if self._loaded:
+            return
+
+        with (files(module) / self._file_name).open() as f:
+            n = 0
+            for line in f:
+                k, v = line.strip().split()
+                self._data[k] = float(v)
+                n += int(v)
+
+        k: str = ""
+        for k, v in self._data.items():
+            self._data[k] = log10(v / n)
+
+        self._ngram_len = len(k)
+        self._floor = log10(0.01 / n)
+        self._loaded = True
+
+    def score(self, text: str) -> float:
+        """
+        Score the text using the ngram data.
+
+        Parameters
+        ----------
+        text : str
+            The text to score.
+
+        Returns
+        -------
+        float
+            The calculated score.
+        """
+        if not self._loaded:
+            self._load_data()
+
+        total = 0.0
+        for i in range(len(text) - self._ngram_len + 1):
+            ngram = text[i : i + self._ngram_len]
+            total += self._data.get(ngram.upper(), self._floor)
+
+        return total
 
 
-def _load_data(
-    file_name: str,
-    *,
-    module: str = "cryptolab.scoring.data",
-) -> _ngram_data:
-    data: dict[str, float] = {}
-
-    with open_text(module, file_name) as f:
-        n = 0
-        for line in f:
-            k, v = line.strip().split()
-            data[k] = float(v)
-            n += int(v)
-
-    for k, v in data.items():
-        data[k] = log10(v / n)
-
-    return data, log10(0.01 / n)
-
-
-def _score(text: str, data: _ngram_data, window: int) -> float:
-    lut, floor = data
-    total = 0.0
-    for i in range(len(text) - window + 1):
-        total += lut.get(text[i : i + window].upper(), floor)
-
-    return total
-
-
-_monogram_data: _ngram_data | None = None
+_monogram = _NgramScorer("english_monograms.txt")
+_bigram = _NgramScorer("english_bigrams.txt")
+_trigram = _NgramScorer("english_trigrams.txt")
+_quadgram = _NgramScorer("english_quadgrams.txt")
+_quintgram = _NgramScorer("english_quintgrams.txt")
 
 
 def monogram_score(text: str) -> float:
-    global _monogram_data
-    if _monogram_data is None:
-        _monogram_data = _load_data("english_monograms.txt")
-    return _score(text, _monogram_data, 1)
+    """
+    Score the text using monogram data.
 
+    Parameters
+    ----------
+    text : str
+        The text to score.
 
-_bigram_data: _ngram_data | None = None
+    Returns
+    -------
+    float
+        The calculated score.
+    """
+    return _monogram.score(text)
 
 
 def bigram_score(text: str) -> float:
-    global _bigram_data
-    if _bigram_data is None:
-        _bigram_data = _load_data("english_bigrams.txt")
-    return _score(text, _bigram_data, 2)
+    """
+    Score the text using bigram data.
 
+    Parameters
+    ----------
+    text : str
+        The text to score.
 
-_trigram_data: _ngram_data | None = None
+    Returns
+    -------
+    float
+        The calculated score.
+    """
+    return _bigram.score(text)
 
 
 def trigram_score(text: str) -> float:
-    global _trigram_data
-    if _trigram_data is None:
-        _trigram_data = _load_data("english_trigrams.txt")
-    return _score(text, _trigram_data, 3)
+    """
+    Score the text using trigram data.
 
+    Parameters
+    ----------
+    text : str
+        The text to score.
 
-_quadgram_data: _ngram_data | None = None
+    Returns
+    -------
+    float
+        The calculated score.
+    """
+    return _trigram.score(text)
 
 
 def quadgram_score(text: str) -> float:
-    global _quadgram_data
-    if _quadgram_data is None:
-        _quadgram_data = _load_data("english_quadgrams.txt")
-    return _score(text, _quadgram_data, 4)
+    """
+    Score the text using quadgram data.
 
+    Parameters
+    ----------
+    text : str
+        The text to score.
 
-_quintgram_data: _ngram_data | None = None
+    Returns
+    -------
+    float
+        The calculated score.
+    """
+    return _quadgram.score(text)
 
 
 def quintgram_score(text: str) -> float:
-    global _quintgram_data
-    if _quintgram_data is None:
-        _quintgram_data = _load_data("english_quintgrams.txt")
-    return _score(text, _quintgram_data, 5)
+    """
+    Score the text using quintgram data.
+
+    Parameters
+    ----------
+    text : str
+        The text to score.
+
+    Returns
+    -------
+    float
+        The calculated score.
+    """
+    return _quintgram.score(text)
 
 
 if __name__ == "__main__":
