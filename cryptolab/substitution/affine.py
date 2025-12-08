157a6df7c876fb36fc7f8e3142dfda4d69ff4fc9
Matthew <mcriley821@gmail.com>
2025-12-07 22:44:47 -0600

    add argparse to affine

--- cryptolab/substitution/affine.py
+++ cryptolab/substitution/affine.py
@@ -2,9 +2,133 @@
 https://en.wikipedia.org/wiki/Affine_cipher
 """
 
+import sys
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from dataclasses import dataclass
+from textwrap import dedent
 from typing import Callable, Iterator
 
 from cryptolab.scoring.ngram import monogram_score
+
+
+@dataclass
+class Arguments:
+    """
+    Arguments needed to execute the affine cipher algorithm.
+
+    Parameters
+    ----------
+    input : str | None
+        Path to an input file to read the text from. If None, reads from stdin.
+
+    key : tuple[int, int]
+        The (a, b) coefficients for the affine algorithm.
+
+    decrypt : bool
+        Whether to decrypt the text.
+
+    preserve : bool
+        Whether to preserve non-alphabeticals in the output.
+    """
+
+    input: str | None
+    key: tuple[int, int]
+    decrypt: bool
+    preserve: bool
+
+
+def main() -> int:
+    """
+    The main function handles parsing the command line arguments and executing
+    the affine cipher with those arguments.
+
+    Returns
+    -------
+    int
+        Return code. 1 if an error occured; 0 on success.
+    """
+    parser = ArgumentParser()
+    configure_parser(parser)
+    args = Arguments(**vars(parser.parse_args()))
+    return execute(args)
+
+
+def configure_parser(parser: ArgumentParser):
+    """
+    Configure the parser for the affine cipher. The parser will then produce
+    a namespace suitable for conversion to affine.Arguments.
+    """
+    parser.formatter_class = RawDescriptionHelpFormatter
+    parser.description = dedent("""\
+    The affine cipher is a monoalphabetic substitution cipher where each letter
+    in the alphabet is encrypted using a modular affine equation:
+
+    (a * x + b) % 26
+
+    The equation must be reversible to decrypt, and thus only a subset of
+    values are suitable foe the "a" coefficient.
+
+    See https://en.wikipedia.org/wiki/Affine_cipher for more info.
+    """)
+    parser.add_argument(
+        "key",
+        type=int,
+        nargs=2,
+        metavar=("a", "b"),
+        help="the key coefficients such that (a*x + b) %% 26",
+    )
+    parser.add_argument(
+        "input",
+        nargs="?",
+        default=None,
+        help="the text file to encrypt or decrypt [default: stdin]",
+    )
+    parser.add_argument(
+        "-d",
+        "--decrypt",
+        action="store_true",
+        default=False,
+        help="decrypt the text [default: %(default)s]",
+    )
+    parser.add_argument(
+        "-p",
+        "--preserve",
+        action="store_true",
+        default=False,
+        help="preserve non-alphabeticals in the output [default: %(default)s]",
+    )
+
+
+def execute(args: Arguments) -> int:
+    """
+    Execute the affine cipher algorithm using the given args.
+
+    Parameters
+    ----------
+    args : Arguments
+        The arguments to the affine cipher algorithm.
+
+    Returns
+    -------
+    int
+        Return code. 1 if an error occurred; 0 on success.
+    """
+    try:
+        if args.input is not None:
+            with open(args.input, "r") as f:
+                text = f.read()
+        else:
+            text = sys.stdin.read()
+
+        if args.decrypt:
+            print(decrypt(text, args.key, preserve_nonalpha=args.preserve))
+        else:
+            print(encrypt(text, args.key, preserve_nonalpha=args.preserve))
+    except Exception as e:
+        print(e)
+        return 1
+
+    return 0
 
 
 def encrypt(
@@ -61,7 +185,12 @@ def encrypt(
     return ret
 
 
-def decrypt(ciphertext: str, key: tuple[int, int]) -> str:
+def decrypt(
+    ciphertext: str,
+    key: tuple[int, int],
+    *,
+    preserve_nonalpha: bool = False,
+) -> str:
     """
     Decrypt the ciphertext using the affine cipher.
 
@@ -77,6 +206,9 @@ def decrypt(ciphertext: str, key: tuple[int, int]) -> str:
     key : tuple[int, int]
         A tuple representing the (a, b) coefficients.
 
+    preserve_nonalpha : bool,default=False
+        Whether to preserve nonalphabetics in the output.
+
     Raises
     ------
     ValueError
@@ -89,14 +221,16 @@ def decrypt(ciphertext: str, key: tuple[int, int]) -> str:
     """
     a, b = key
     if a not in (1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25):
-        raise ValueError("coefficient `a` must be coprime to 26")
+        msg = "coefficient `a` must be coprime to 26"
+        raise ValueError(msg)
 
     ret = ""
     a_inv = pow(a, -1, 26)
 
     for c in ciphertext:
         if not c.isalpha():
-            ret += c
+            if preserve_nonalpha:
+                ret += c
             continue
 
         offset = ord("A") if c.isupper() else ord("a")
@@ -160,26 +294,4 @@ def crack(
 
 
 if __name__ == "__main__":
-    from random import choice, randint
-
-    plaintext = "Defend the east wall of the castle."
-
-    a = choice((1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25))
-    b = randint(1, 25)
-
-    key = (a, b)
-    print(f"key: {key}")
-
-    enc = encrypt(plaintext, key, preserve_nonalpha=True)
-    print(enc, "\n")
-
-    dec = decrypt(enc, key)
-    print(dec, "\n")
-
-    assert plaintext == dec
-
-    cracked = crack(enc)
-    print(cracked, "\n")
-
-    assert plaintext == cracked[0]
-    assert key == cracked[1]
+    sys.exit(main())
